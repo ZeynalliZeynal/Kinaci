255187906394ccc2820145d06233e2fe1789dc93
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-03-09 22:31:56 +0400

    search setup

--- src/components/search/searchContainer/index.jsx
+++ src/components/search/searchContainer/index.jsx
@@ -1,8 +1,16 @@
 import SelectCountry from '~/components/search/selectCountry/index.jsx'
+import { useState } from 'react'
+import classNames from 'classnames'
 
 export default function SearchContainer() {
+  const [activeButton, setActiveButton] = useState('all')
+
+  function handleSetActive(btnValue) {
+    setActiveButton(btnValue.dataset.label)
+  }
+
   return (
-    <div className="menu-dropdown">
+    <div className="search-container relative w-fit grid gap-5">
       <SelectCountry />
     </div>
   )
---
+++ src/components/search/selectTabBtns/index.jsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames'
+
+export default function SelectTabBtns() {
+  return (
+    <div className="tab-btns text-blue-900 font-semibold text-md flex rounded-t-selectBtn overflow-hidden">
+      <button
+        onClick={(e) => handleSetActive(e.currentTarget)}
+        className="w-[100px] bg-gray-100 px-7 py-4 active:transform-none relative"
+        data-label="all"
+      >
+        <span className="z-[5] relative">Hamısı</span>
+      </button>
+      <button
+        onClick={(e) => handleSetActive(e.currentTarget)}
+        className="w-[100px] px-7 py-4 bg-gray-100 active:transform-none relative"
+        data-label="rent"
+      >
+        <span className="z-[5] relative">İcarə</span>
+      </button>
+      <button
+        onClick={(e) => handleSetActive(e.currentTarget)}
+        className="w-[100px] px-7 py-4 bg-gray-100 active:transform-none relative"
+        data-label="sale"
+      >
+        <span className="z-[5] relative">Satılır</span>
+      </button>
+      <div
+        className={classNames(
+          "flex justify-center moving-box w-[100px] h-[54px] absolute z-[4] bg-white after:content-[''] after:absolute after:bottom-0 after:w-[50%] after:h-0.5 after:bg-orange-500 rounded-t-selectBtn transition-transform",
+          {
+            'translate-x-0': activeButton === 'all',
+            'translate-x-[100px]': activeButton === 'rent',
+            'translate-x-[200px]': activeButton === 'sale',
+          },
+        )}
+      ></div>
+    </div>
+  )
+}
