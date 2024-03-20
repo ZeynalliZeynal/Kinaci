4bc50ff71c6159ead901a0f418532d2d72a90e89
Zeyna104 <zzeynalli446@gmail.com>
2024-03-20 04:12:52 +0400

    tool btns

--- src/pages/estate/estateItem/index.jsx
+++ src/pages/estate/estateItem/index.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import Loader from '~/components/loader.jsx'
+import { GoPlay, GoZoomIn } from 'react-icons/go'
 
 export default function EstateItem() {
   const [estateItem, setEstateItem] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
+  const [imageIndex, setImageIndex] = useState(0)
 
   const { id } = useParams()
   console.log(id)
@@ -33,11 +35,38 @@ export default function EstateItem() {
       ) : (
         <section>
           <div className="container">
-            <div className="grid grid-cols-[7fr_5fr]">
+            <div className="grid grid-cols-[8fr_4fr]">
               <div className="grid gap-3 text-blue-900">
-                <h2 className="text-5xl font-semibold">{estateItem?.title}</h2>
-                <p className="text-md">{estateItem?.location.city}</p>
+                <div className="header">
+                  <h2 className="text-5xl font-semibold">
+                    {estateItem?.title}
+                  </h2>
+                  <p className="text-md">{estateItem?.location.city}</p>
+                </div>
+                <div className="slider w-full">
+                  <div className="img-container">
+                    <img
+                      src={estateItem?.assets?.img[imageIndex]}
+                      alt="Carousel"
+                    />
+                  </div>
+                  <div className="w-full bg-gradient-to-b from-[#fefefe] to-[#ededed] text-[#039] grid grid-cols-2 py-2">
+                    <button className="py-2.5 font-medium flex gap-2 border-r border-[#ddd]">
+                      <span className="size-4">
+                        <GoZoomIn />
+                      </span>
+                      Böyük Foto
+                    </button>
+                    <button className="py-2.5 font-medium flex gap-2 border-l border-white">
+                      <span className="size-4">
+                        <GoPlay />
+                      </span>
+                      Video
+                    </button>
+                  </div>
+                </div>
               </div>
+              <div></div>
             </div>
           </div>
         </section>
