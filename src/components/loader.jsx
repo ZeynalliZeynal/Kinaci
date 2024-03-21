db104c5bb4e458441cf3d00f4aacc7c692e883c3
Zeyna104 <zzeynalli446@gmail.com>
2024-03-21 15:04:25 +0400

    dsad

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,5 +1,5 @@
 import Lottie from 'react-lottie'
-import animationData from '~/assets/img/loading.json'
+import animationData from '~/assets/img/loading.gif'
 
 const defaultOptions = {
   loop: true, // Set to true for continuous looping
@@ -10,6 +10,7 @@ const defaultOptions = {
 export default function Loader() {
   return (
     <section className="size-[500px] grid justify-center w-full pointer-events-none">
+      <img src={animationData} alt="Loading" />
       <Lottie options={defaultOptions} />
     </section>
   )
