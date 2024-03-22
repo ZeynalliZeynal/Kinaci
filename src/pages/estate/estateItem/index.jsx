97e8eaed318de380c59a70d1e86ce07de2b8afff
Zeyna104 <zzeynalli446@gmail.com>
2024-03-22 17:07:19 +0400

    "Detaylar" button - scroll to top

--- src/pages/estate/estateItem/index.jsx
+++ src/pages/estate/estateItem/index.jsx
@@ -1,6 +1,15 @@
 import EstateInfo from '~/pages/estate/estateItem/estateInfo/index.jsx'
+import { useEffect, useState } from 'react'
 
 export default function EstateItem() {
+  const [scrollToTop, setScrollToTop] = useState(true)
+
+  useEffect(() => {
+    if (scrollToTop) {
+      window.scrollTo(0, 0)
+      setScrollToTop(false) // Reset flag to prevent infinite looping
+    }
+  }, [scrollToTop])
   return (
     <main className="bg-gray-100">
       <EstateInfo />
