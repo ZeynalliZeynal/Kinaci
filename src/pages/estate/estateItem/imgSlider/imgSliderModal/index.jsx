58596e93362f336aed6e1027e40340e5650b39ba
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-04-10 18:13:31 +0400

    modal swiper

--- src/pages/estate/estateItem/imgSlider/imgSliderModal/index.jsx
+++ src/pages/estate/estateItem/imgSlider/imgSliderModal/index.jsx
@@ -1,6 +1,16 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment } from 'react'
 import SwiperCarousel from '~/pages/estate/estateItem/imgSlider/imgSliderModal/swiperCarousel/index.jsx'
+import { GrClose } from 'react-icons/gr'
+
+const modalTransition = {
+  enter: 'ease-out duration-300',
+  enterFrom: 'opacity-0',
+  enterTo: 'opacity-100',
+  leave: 'ease-in duration-200',
+  leaveFrom: 'opacity-100',
+  leaveTo: 'opacity-0',
+}
 
 export default function ImgSliderModal({
   isModalOpen,
@@ -11,17 +21,13 @@ export default function ImgSliderModal({
 }) {
   return (
     <Transition appear show={isModalOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={closeModal}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-black/25" />
+      <Dialog
+        as="div"
+        className="relative z-10 w-full h-full"
+        onClose={closeModal}
+      >
+        <Transition.Child as={Fragment} {...modalTransition}>
+          <div className="fixed inset-0 bg-black/25 backdrop-blur-sm" />
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
@@ -36,7 +42,20 @@ export default function ImgSliderModal({
               leaveTo="opacity-0 scale-95"
             >
               <Dialog.Panel className="w-screen transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all relative">
-                <SwiperCarousel estateItem={estateItem} />
+                <SwiperCarousel
+                  estateItem={estateItem}
+                  imageIndex={imageIndex}
+                  setImageIndex={setImageIndex}
+                />
+
+                <button
+                  className="absolute z-20 top-8 rounded-lg bg-blue-900/10 p-2 right-8 text-blue-900 hover:bg-blue-900/5"
+                  onClick={closeModal}
+                >
+                  <span className="size-4">
+                    <GrClose />
+                  </span>
+                </button>
               </Dialog.Panel>
             </Transition.Child>
           </div>
--- src/pages/estate/estateItem/imgSlider/imgSliderModal/swiperCarousel/index.jsx
+++ src/pages/estate/estateItem/imgSlider/imgSliderModal/swiperCarousel/index.jsx
@@ -1,27 +1,59 @@
-import { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Thumbs } from 'swiper/modules'
+import { Mousewheel, Navigation, Pagination, Zoom } from 'swiper/modules'
 
 import 'swiper/css'
 import 'swiper/css/zoom'
 import 'swiper/css/pagination'
 import 'swiper/css/thumbs'
 
-export default function SwiperCarousel({ estateItem }) {
-  const [thumbsSwiper, setThumbsSwiper] = useState(null)
+export default function SwiperCarousel({
+  estateItem,
+  imageIndex,
+  setImageIndex,
+}) {
+  const handleSlideChange = (swiper) => {
+    setImageIndex(swiper.activeIndex)
+  }
+
   return (
-    <Swiper
-      className="w-full h-full"
-      spaceBetween={10}
-      navigation={true}
-      thumbs={{ swiper: thumbsSwiper }}
-      modules={[Thumbs]}
-    >
-      {estateItem?.assets?.img.map((imgUrl, i) => (
-        <SwiperSlide key={imgUrl}>
-          <img src={imgUrl} alt={`Image ${i + 1}`} className="object-contain" />
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <div className="w-full h-full relative">
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#000',
+          '--swiper-pagination-color': '#FF5300FF',
+        }}
+        className="w-full h-full"
+        spaceBetween={10}
+        direction={'horizontal'}
+        slidesPerView={1}
+        mousewheel={true}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        rewind={true}
+        zoom={true}
+        modules={[Navigation, Zoom, Mousewheel, Pagination]}
+        onSlideChange={handleSlideChange}
+      >
+        {estateItem?.assets.img.map((imgUrl, i) => (
+          <SwiperSlide key={imgUrl}>
+            <div className="swiper-zoom-container bg-neutral-300">
+              <img
+                src={imgUrl}
+                alt={`Image ${i + 1}`}
+                className="object-contain"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="absolute bottom-8 w-full grid justify-center z-50 text-blue-900">
+        <div className="w-fit p-2 text-xs bg-white rounded-xl">
+          <span>{imageIndex + 1}</span> /{' '}
+          <span>{estateItem?.assets.img.length}</span>
+        </div>
+      </div>
+    </div>
   )
 }
