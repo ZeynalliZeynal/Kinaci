af096613e672e10de75e63794e228546f5a15c4d
MuradNuh-zada <muradnuhzada@gmail.com>
2024-04-16 00:12:24 +0400

    Comment skeleton length 4

--- src/pages/comments/CommentSkeleton.jsx
+++ src/pages/comments/CommentSkeleton.jsx
@@ -3,7 +3,7 @@ import Skeleton from '~/components/Skeleton.jsx'
 export default function CommentSkeleton() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px] h-full">
-      {Array.from({ length: 10 }, (_, i) => (
+      {Array.from({ length: 4 }, (_, i) => (
         <div className="grid grid-cols-[160px_1fr] gap-6 h-[300px]" key={i}>
           <div className="size-[160px] rounded-full overflow-hidden">
             <Skeleton className="p-6 size-full" />
