29d48242e723f58c9120e26c516c749403a657e3
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-04-20 13:46:27 +0400

    images update

--- src/data/bannerCarouselImages.js
+++ src/data/bannerCarouselImages.js
@@ -1,20 +1,16 @@
-import image1 from '~/assets/img/carousel-images/image1.jpg'
-import image2 from '~/assets/img/carousel-images/image2.jpg'
-import image3 from '~/assets/img/carousel-images/image3.jpg'
-
 export const bannerCarouselImages = [
   {
-    src: image1,
+    src: 'https://images.unsplash.com/photo-1489370321024-e0410ad08da4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Keyfiyyətə Zəmanət Verən Obyektlər',
     desc: 'Kınacı Qrup tikinti və daşınmaz əmlak xidmətləri sektorunda 30 illik təcrübəyə malik şirkətlər qrupudur.',
   },
   {
-    src: image2,
+    src: 'https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Lorem ipsum dolor sit.',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolorum labore qui reiciendis rem suscipit?',
   },
   {
-    src: image3,
+    src: 'https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     title: 'Lorem ipsum dolor sit.',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dolorum labore qui reiciendis rem suscipit?',
   },
