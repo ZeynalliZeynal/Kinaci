c0e16316f5428072db7cc9b956733c8856cd91e0
Zeyna104 <zzeynalli446@gmail.com>
2024-05-16 10:47:42 +0400

    add

--- src/reducers/commentsReducer.js
+++ src/reducers/commentsReducer.js
@@ -7,34 +7,33 @@ export const initialState = {
   countSymbols: 500,
   comments: [],
   isChecked: false,
-}
+};
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_VALUES':
-      return { ...state, values: { ...state.values, ...action.payload } }
+      return { ...state, values: { ...state.values, ...action.payload } };
     case 'IS_CHECKED':
-      return { ...state, isChecked: !state.isChecked }
+      return { ...state, isChecked: !state.isChecked };
     case 'SET_COMMENTS':
-      return { ...state, comments: action.payload }
+      return { ...state, comments: action.payload };
     case 'RESET_VALUES':
       return {
         ...initialState,
-        isChecked: state.isChecked,
         comments: state.comments,
-      }
+      };
     case 'SAVE_LOCAL_STORAGE':
-      localStorage.setItem('commentValues', JSON.stringify(action.payload))
-      return state
+      localStorage.setItem('commentValues', JSON.stringify(action.payload));
+      return state;
     case 'REMOVE_LOCAL_STORAGE':
-      localStorage.removeItem('commentValues')
-      return state
+      localStorage.removeItem('commentValues');
+      return state;
     case 'COUNT_SYMBOLS':
       return {
         ...state,
         countSymbols: initialState.countSymbols - action.payload,
-      }
+      };
     default:
-      return state
+      return state;
   }
-}
+};
