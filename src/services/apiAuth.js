0737c996260a4622e4fb02c4aa1959397a86f6df
ZeynalliZeynal <zzeynalli446@gmail.com>
2024-07-20 03:18:28 +0400

    supabase setup

---
+++ src/services/apiAuth.js
@@ -0,0 +1,17 @@
+import { supabase } from '~/services/supabase.js';
+
+export const signUp = async ({ fullName, email, password }) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
